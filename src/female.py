05264aa0eda4575a5ff24f5c55c8e82040b4aa1e
arthurhalet <arthurhalet@MacBook-de-timoun.local>
2012-01-21 17:14:41 +0100

    +ajout de tableau de male +modification du tableau de frog qui prend le tableau de male et de femelle +Utilisation de la variable du nombre de grenouille males -modification nom loadFrogs en loadMaleFrogs

---
+++ src/female.py
@@ -0,0 +1,37 @@
+ 
+# -*- coding: utf-8 -*-
+
+import breve
+from random import uniform, randint
+
+class Female(breve.Mobile):
+    numFrog = 0
+	
+    def __init__(self):
+        breve.Female.__init__(self)
+        self.encounteredPreys, self.encounteredPredators, self.totalEnergyBoost = 0, 0, 0
+        self.init()
+
+    def init(self):
+        self.move( breve.vector(uniform(-8, 8), uniform(-8, 8), 0.01) )
+        self.setShape( breve.createInstances(breve.Cube, 1).initWith( breve.vector(0.1, 0.1, 0.1)))
+        self.setColor( breve.randomExpression( breve.vector( 1, 1, 1 ) ) )
+		
+    def iterate(self):
+        self.setVelocity( self.controller.selectMovement(self.getId()) ) 
+
+    def getId(self):
+        return self.id
+
+    def getEnvironment(self):
+        return self.controller.getEnvironment(self.controller.worldToImage(self.getLocation()))
+
+    def getSoundLevel(self):
+        return self.controller.getSoundLevel(self.controller.worldToImage(self.getLocation()))
+
+    def __str__(self):
+        pos = self.controller.worldToImage(self.getLocation())
+        env = self.getEnvironment().getName()
+        return 'Frog #%d  energy:%d location:%d,%d  env:%s' % (self.id, self.energy, pos.x, pos.y, env)
+
+breve.Female = Female
