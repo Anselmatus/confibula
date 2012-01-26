33760231d32b54d9dace94ccd9077befc5d4417f
guiiigui <guiiigui@debian>
2012-01-26 20:48:47 +0100

    + premiere gestion de foret

--- src/movement.py
+++ src/movement.py
@@ -26,22 +26,22 @@ class Movement(breve.Abstract):
             elif self.getFrog(id).state == 'hunting':
                 return self.hunter(id)
 
-	if isinstance(self.getFrog(id), breve.Female) :
+	if isinstance(self.getFrog(id), breve.Female):
 
-            if self.getFrog(id).state == 'findPartener' :
+            if self.getFrog(id).state == 'findPartener':
                 return self.findPartner(id)
-	    elif self.getFrog(id).state == 'hunting' :
+	    elif self.getFrog(id).state == 'hunting':
                 return self.hunter(id)
-            else :
+            else:
 		return self.randomMovement(id)
 
     def hunter(self, id):
         env = self.getEnvironment(id)
         
         if (self.getFrog(id).energy > (self.getFrog(id).maxEnergy / 100.) * 1000):
-            if isinstance(self.getFrog(id), breve.Female) :
+            if isinstance(self.getFrog(id), breve.Female):
                 self.getFrog(id).state = 'findPartener'
-            elif isinstance(self.getFrog(id), breve.Male) :
+            elif isinstance(self.getFrog(id), breve.Male):
                 self.getFrog(id).state = 'moveToSing'
 
 
@@ -49,7 +49,7 @@ class Movement(breve.Abstract):
             self.getFrog(id).energy += 10
             self.getFrog(id).state == 'hunting'
 
-        elif isinstance(self.getFrog(id), breve.Male) :
+        elif isinstance(self.getFrog(id), breve.Male):
             self.getFrog(id).energy += 1
             self.getFrog(id).state == 'hunting'
 
@@ -84,59 +84,63 @@ class Movement(breve.Abstract):
 #	return self.randomMovement(id)
 
     def moveToSing(self, id):
-        speed = float(self.getFrog(id).energy)/1000
+        speed = float(self.getFrog(id).energy) / 1000
         location = self.getFrog(id).getLocation()
         soundLevel = self.controller.getSoundLevel(location)
         dbMaxToSing = self.controller.config.getValue("dbMaxToSing")
         env = self.getEnvironment(id).getName()
-        if( ( ( soundLevel > dbMaxToSing -5 and soundLevel < dbMaxToSing ) or soundLevel == 0 ) and env == 'Eau') :
+        if(((soundLevel > dbMaxToSing -5 and soundLevel < dbMaxToSing) or soundLevel == 0) and env == 'Eau'):
 
             self.getFrog(id).state = 'singing'
             return breve.vector(0, 0, 0)
 
-        else : # deplacements
-            self.getFrog(id).energy -= speed/2
-            if (env != 'Eau' or soundLevel >= dbMaxToSing) :
+        else: # deplacements
+            self.getFrog(id).energy -= speed / 2
+            if (env != 'Eau' or soundLevel >= dbMaxToSing):
                 moveField = self.getMoveField(location, speed)
+            if(env != 'Eau' and soundLevel == 0):
+                direction = self.moveTo(location, self.controller.getNearestWater(location), speed)
+                if soundLevel >= dbMaxToSing:
+                    self.unLockFrog(direction)
+            elif(soundLevel < dbMaxToSing):
+                return self.moveTo(location, self.controller.getSoundSource(), speed)
+            else: # to close to sing
                 min = moveField[0]
                 middle = dbMaxToSing-2.5
-                for point in moveField[1:] :
-                    if( (self.controller.getSoundLevel(point)-middle)**2 < (self.controller.getSoundLevel(min)-middle)**2 ) :
+                for point in moveField[1:]:
+                    if((self.controller.getSoundLevel(point)-middle) ** 2 < (self.controller.getSoundLevel(min)-middle) ** 2):
                         min = point
                 return min - breve.vector(location.x, location.y, 0)
-            
-            elif(soundLevel < dbMaxToSing) :
-                return self.moveTo(location, self.controller.getSoundSource(), speed)
 
     def singer(self, id):
-        if (self.getFrog(id).energy <= (self.getFrog(id).minEnergy/100.)*1000 ) :
+        if (self.getFrog(id).energy <= (self.getFrog(id).minEnergy / 100.) * 1000):
             self.getFrog(id).state = 'hunting'
-        else :
+        else:
             self.getFrog(id).energy -= 1
         return breve.vector(0, 0, 0)
 
     def findPartner(self, id):
 	female = self.getFrog(id)
 	location = female.getLocation()
-	speed = float(female.energy)/2000
-	viewMale =female.viewMale()
+	speed = float(female.energy) / 2000
+	viewMale = female.viewMale()
 	moveField = self.getMoveField(location, speed)
 	if self.controller.getSoundLevel(location):
-            if viewMale != 0 :
-		return self.partnerChoice(viewMale,id,speed)
+            if viewMale != 0:
+		return self.partnerChoice(viewMale, id, speed)
             else:
-		maxDB= 0
+		maxDB = 0
 		dot = None
 		for dot in moveField:
-			if maxDB < self.controller.getSoundLevel(dot):
-				maxDB = self.controller.getSoundLevel(dot)
-				dotChoice = dot
+                    if maxDB < self.controller.getSoundLevel(dot):
+                        maxDB = self.controller.getSoundLevel(dot)
+                        dotChoice = dot
 		return dot
         else:
-		female.state = 'hunter'
-		return self.hunter(id)
+            female.state = 'hunter'
+            return self.hunter(id)
 
-    def partnerChoice(self,listPartner,id, speed):#choisis un partner en fonction du tableau de male passé en parametre
+    def partnerChoice(self, listPartner, id, speed):#choisis un partner en fonction du tableau de male passé en parametre
 	malePower = listPartner[0].voicePower + listPartner[0].voiceQuality + listPartner[0].throatColor
 	maleChoice = listPartner[0]
 	for male in listPartner[1:]:
@@ -146,19 +150,34 @@ class Movement(breve.Abstract):
 	return self.moveTo(self.getFrog(id).getLocation(), maleChoice.getLocation(), speed)
 
     def randomMovement(self, id):
-        speed = float(self.getFrog(id).energy)/2000
+        speed = float(self.getFrog(id).energy) / 2000
         x, y = uniform(-speed, speed), uniform(-speed, speed)
-        self.getFrog(id).energy -= ( x ** 2 + y ** 2 ) / 2
+        self.getFrog(id).energy -= (x ** 2 + y ** 2) / 2
 	return breve.vector(x, y, 0)
-
 
     def cheater(self):
 	return 0
+    
+    def unLockFrog(self, direction):
+        pointReturned = self.rotation(120, direction)
+        if self.controller.getSoundLevel(pointReturned) >= dbMaxToSing:
+            return self.unLockFrog(pointReturned)
+        else:
+            return pointReturned
 
-    def moveTo(self, location, destination, speed):
+    def rotation(self, angle, direction):
+        cosAngle = cos(80)
+        sinAngle = sin(80)
+        newX = direction.x * cosAngle-direction.y * sinAngle
+        newY = direction.y * cosAngle + direction.x * sinAngle
+        direction.x = newX
+        direction.y = newY
+        return direction
+        
+    def moveTo(self, location, destination, speed):    
         direction = destination - location
-        distance = sqrt( direction.x**2 +  direction.y**2  )
-        return (direction/distance)*speed
+        distance = sqrt(direction.x ** 2 + direction.y ** 2)
+        return (direction / distance) * speed
 
     def getEnvironment(self, id):
         return self.controller.getEnvironment(self.controller.worldToImage(self.getFrog(id).getLocation()))
@@ -168,14 +187,14 @@ class Movement(breve.Abstract):
 
     def getMoveField(self, location, speed):
         moveField = [] # array of point around the frog
-        moveField.append( breve.vector(location.x + speed, location.y, 0) )
-        moveField.append( breve.vector(location.x - speed, location.y, 0) )
-        moveField.append( breve.vector(location.x, location.y + speed, 0) )
-        moveField.append( breve.vector(location.x, location.y - speed, 0) )
-        moveField.append( breve.vector(location.x + cos(pi/4)*speed, location.y + sin(pi/4)*speed, 0) )
-        moveField.append( breve.vector(location.x + cos(pi/4)*speed, location.y - sin(pi/4)*speed, 0) )
-        moveField.append( breve.vector(location.x - cos(pi/4)*speed, location.y + sin(pi/4)*speed, 0) )
-        moveField.append( breve.vector(location.x - cos(pi/4)*speed, location.y - sin(pi/4)*speed, 0) )
+        moveField.append(breve.vector(location.x + speed, location.y, 0))
+        moveField.append(breve.vector(location.x - speed, location.y, 0))
+        moveField.append(breve.vector(location.x, location.y + speed, 0))
+        moveField.append(breve.vector(location.x, location.y - speed, 0))
+        moveField.append(breve.vector(location.x + cos(pi / 4) * speed, location.y + sin(pi / 4) * speed, 0))
+        moveField.append(breve.vector(location.x + cos(pi / 4) * speed, location.y - sin(pi / 4) * speed, 0))
+        moveField.append(breve.vector(location.x - cos(pi / 4) * speed, location.y + sin(pi / 4) * speed, 0))
+        moveField.append(breve.vector(location.x - cos(pi / 4) * speed, location.y - sin(pi / 4) * speed, 0))
         return moveField
 
 
