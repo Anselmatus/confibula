fffab11d1dad4e7bc9d445b47b81c49735f923ce
guiiigui <guiiigui@debian>
2012-01-29 00:30:59 +0100

    + creation classe de chant de confibula.py + creation classe de texture dynamic pour la classe de chant + loadSongField + derni?res finissions de movetosing ;) (merci de rapporter les bugs des d?placements males !! )
    => nous remarquons que ?a fait un peu laguer, ( on peux regler la pr?cision du chant ), donc il me semble pertinant de mettre un bool dans le fichier de config pour determiner ou non si on affiche les chants !!!

---
+++ src/female.py
@@ -0,0 +1,41 @@
+ 
+# -*- coding: utf-8 -*-
+
+from random import randint
+from random import uniform
+
+import breve
+import frog
+
+class Female(breve.Frog):
+	
+    def __init__(self):
+        breve.Frog.__init__(self)
+        self.encounteredPreys, self.encounteredPredators, self.totalEnergyBoost = 0, 0, 0
+        self.state = 'findPartener'
+        self.init()
+
+    def init(self):
+        width = self.controller.config.getValue("mapWidth")/2
+        height = self.controller.config.getValue("mapHeight")/2
+        self.move(breve.vector(uniform(-width, width), uniform(-height, height), 0.01))
+        self.setShape(breve.createInstances(breve.Cube, 1).initWith(breve.vector(0.1, 0.1, 0.1)))
+        self.setColor(breve.vector(1, 0, 0))
+
+    def iterate(self):
+        breve.Frog.iterate(self)
+        if self.state == 'coupling' :
+            self.setShape(breve.createInstances(breve.Cube, 1).initWith(breve.vector(0.15, 0.15, 0.15)))
+            self.setColor(breve.vector(0, 1, 1))
+        else :
+            self.setShape(breve.createInstances(breve.Cube, 1).initWith(breve.vector(0.1, 0.1, 0.1)))
+            self.setColor(breve.vector(1, 0, 0))
+
+    def __str__(self):
+        pos = self.controller.worldToImage(self.getLocation())
+        env = self.getEnvironment().getName()
+        return 'Frog #%d  energy:%d location:%d,%d  env:%s  state:%s' % (self.id, self.energy, pos.x, pos.y, env, self.state)
+
+    
+
+breve.Female = Female
